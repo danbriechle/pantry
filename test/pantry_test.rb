4d58ebd54823911bdfa1e912c43091955ba0b0e2
Daniel Briechle <danbriechle@hotmail.com>
2018-10-16 19:24:22 -0600

    Created pantry class with stock check and restock methods, added corresponding minitest

--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -4,4 +4,31 @@ require 'minitest/pride'
 
 class PantryTest < Minitest::Test
 
+  def test_it_exists
+    pantry = Pantry.new
+    assert_instance_of Pantry, pantry
+  end
+
+def test_it_has_a_stock
+  pantry = Pantry.new
+  assert_instance_of Hash, pantry.stock
+end
+
+def test_stock_has_default_value
+  pantry = Pantry.new
+  assert_equal 0, pantry.stock_check("Cheese")
+end
+
+def test_it_can_be_restocked
+  pantry = Pantry.new
+  pantry.restock("Cheese", 10)
+  assert_equal 10, pantry.stock_check("Cheese")
+end
+
+def test_stock_can_be_added_to
+  pantry = Pantry.new
+  pantry.restock("Cheese", 10)
+  pantry.restock("Cheese", 20)
+  assert_equal 30, pantry.stock_check("Cheese")
+end
 end
